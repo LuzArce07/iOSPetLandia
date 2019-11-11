bf6e7ee26a4b79391dc853d18d86837d4099ec87
Luz <ulsa@krlos.local>
2019-11-11 15:05:02 -0700

    Interfaz planeada

---
+++ Petlandia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Petlandia
+//
+//  Created by Alumno on 11/11/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
